3046b93895a25904f4d35b8036a51793b3a519b7
bprlab <bprlab@10.200.144.26>
2019-12-19 20:31:04 +0300

    FinalDers5

---
+++ src/main/java/org/medipol/yazilimaraclari/FinalDers5/FinalDers5Application.java
@@ -0,0 +1,13 @@
+package org.medipol.yazilimaraclari.FinalDers5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalDers5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalDers5Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/medipol/yazilimaraclari/FinalDers5/FinalDers5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.medipol.yazilimaraclari.FinalDers5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalDers5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
